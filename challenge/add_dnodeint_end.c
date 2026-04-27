c1042c4589e41a3ccfd3e235a9cab01b672019dc
thekra-alh <202222146@yu.sa.com>
2026-04-27 18:36:37 +0000

    s

--- challenge/add_dnodeint_end.c
+++ challenge/add_dnodeint_end.c
@@ -1,36 +1,29 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list
- * @head: Double pointer to the head of the list
- * @n: Value to store in the new node
- * Return: Address of the new element, or NULL if it failed
- */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *current;
+    dlistint_t *new = malloc(sizeof(dlistint_t));
+    dlistint_t *last = *head;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
+    if (!new)
         return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+    new->n = n;
+    new->next = NULL;
 
-    if (*head == NULL)
+    if (!*head)
     {
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
+        new->prev = NULL;
+        *head = new;
+        return (new);
     }
 
-    current = *head;
-    while (current->next != NULL)
-        current = current->next;
+    while (last->next)
+        last = last->next;
 
-    current->next = new_node;
-    new_node->prev = current;
+    last->next = new;
+    new->prev = last;
 
-    return (new_node);
+    return (new);
 }
